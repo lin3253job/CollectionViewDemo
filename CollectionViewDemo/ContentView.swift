5ec1aeef3ec1318c3198a7a2eaecb82571c6ee96
peterpan <apppeterpan@gmail.com>
2020-09-03 10:27:09 +0800

    Initial Commit

---
+++ CollectionViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CollectionViewDemo
+//
+//  Created by SHIH-YING PAN on 2020/9/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CollectionViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by SHIH-YING PAN on 2020/9/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
